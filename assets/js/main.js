8b0d6c7e18e4e602c0f41387be3c554df39926f0
Royden <jaydendane2@gmail.com>
2021-03-01 16:06:28 +0200

    Just pushing the front end parts of my EOM

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// FOR THE MOVEMENT OF THE SLIDES OF THE CARDS
+// FROM LEFT TO RIGHT
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
